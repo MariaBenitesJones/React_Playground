bed97de4afff78148cde96f902dba072967de3df
MariaBenitesJones <mariaeduardabmrj@gmail.com>
2024-05-16 23:31:17 -0300

    React_Playground inicializado

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Contador from './pages/contador/Contador'
+import Home from './pages/home/Home'
+import Tarefa from './pages/tarefa/Tarefa'
+
+function App() {
+
+  return (
+    <>
+        {/* <Home 
+        titulo=' Componente'
+        texto='Este é um Componente que recebe Props.'
+        />
+        <Contador/> */}
+
+        <Tarefa/>
+    </>
+  )
+}
+
+export default App
---
+++ src/pages/tarefa/Tarefa.tsx
@@ -0,0 +1,33 @@
+﻿import { useEffect, useState } from "react"
+
+function Tarefa() {
+
+  const [completed, setCompleted] = useState (false);
+  const [tarefa, setTarefa] = useState('');
+
+  useEffect(() => {
+    if (completed){
+      setTarefa('Parabens! tarefa concluida!')
+    }
+    else{
+      setTarefa("")
+    }
+  }, [completed])
+
+  console.log("Completed:"+completed);
+
+  return (
+    <div>
+      <h2>Componente Tarefa</h2>
+      
+      <h3>{tarefa}</h3>
+
+      <p>Conclua a tarefa</p>
+
+      <button onClick={() => setCompleted(!completed)}>Concluir tarefa</button>
+      
+      </div>
+  )
+}
+
+export default Tarefa
